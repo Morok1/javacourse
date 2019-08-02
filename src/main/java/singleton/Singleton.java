44d545a4eb3ff8ae72c0e5722d2369024ff5f885
zhenyasmagin@yandex.ru <zhenyasmagin@yandex.ru>
2019-08-02 10:42:19 +0300

    add test singletom

--- src/main/java/singleton/Singleton.java
+++ src/main/java/singleton/Singleton.java
@@ -1,5 +1,19 @@
 package singleton;
 
 public class Singleton {
+    private Singleton() {
+    }
 
+    private static Singleton INSTANCE;
+
+    public static Singleton getSingleton() {
+        if (INSTANCE == null) {
+            INSTANCE = new Singleton();
+        }
+        return INSTANCE;
+    }
+
+    public void printText() {
+        System.out.println("Hello im singleton!");
+    }
 }
---
+++ src/test/java/singleton/SingletonTest.java
@@ -0,0 +1,21 @@
+package singleton;
+
+
+import org.junit.Before;
+
+public class SingletonTest {
+    private Singleton singleton;
+    @Before
+    public void setUp(){
+    }
+
+    @org.junit.Test
+    public void getSingleton() {
+        singleton = Singleton.getSingleton();
+        singleton.printText();
+    }
+
+    @org.junit.Test
+    public void printText() {
+    }
+}
