be7c9cbcb2790a02f5b3b1798b80e46503710722
zhenyasmagin@yandex.ru <zhenyasmagin@yandex.ru>
2019-08-19 10:28:08 +0300

    add small example on stream

---
+++ stream/src/main/java/com/smagin/stream/CreationStream.java
@@ -0,0 +1,9 @@
+package com.smagin.stream;
+
+import java.util.stream.Stream;
+
+public class CreationStream {
+    public static void main(String[] args) {
+        Stream<String> stringStream = Stream.of("as", "as");
+    }
+}
