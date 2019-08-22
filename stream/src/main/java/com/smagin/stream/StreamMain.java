1c2eab7649ffea7ef15f2eefdccffe13fa859b82
zhenyasmagin@yandex.ru <zhenyasmagin@yandex.ru>
2019-08-23 00:01:14 +0300

    add collection

--- stream/src/main/java/com/smagin/stream/StreamMain.java
+++ stream/src/main/java/com/smagin/stream/StreamMain.java
@@ -1,12 +1,17 @@
 package com.smagin.stream;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class StreamMain {
     public static void main(String[] args) {
+        List<Integer>  integers = new ArrayList<>();
+
         //Sources
         int[] array = new int[2];
         Stream.of(array);
+
 
     }
 }
