8bd20a912b13418809fa307908f66514cba51179
zhenyasmagin@yandex.ru <zhenyasmagin@yandex.ru>
2019-08-22 23:58:15 +0300

    add array to Stream

--- stream/src/main/java/com/smagin/stream/StreamMain.java
+++ stream/src/main/java/com/smagin/stream/StreamMain.java
@@ -6,6 +6,7 @@ public class StreamMain {
     public static void main(String[] args) {
         //Sources
         int[] array = new int[2];
-        Stream.of();
+        Stream.of(array);
+
     }
 }
